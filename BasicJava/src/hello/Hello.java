02ebe36342997122f079e1099bd475403bfa8c17
Jet <tong25482015@gmail.com>
2026-06-02 13:10:57 +0700

    Add files via upload

---
+++ BasicJava/src/hello/Hello.java
@@ -0,0 +1,21 @@
+package hello;
+
+public class Hello {
+    public static void main (String[] args){
+        int a =65;
+        char c = 'c';
+        byte b = 5;
+        short s =56;
+        long l = 555555;
+        double d =5.39329439;
+        float f = 6.423f;
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
